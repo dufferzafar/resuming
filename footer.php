11947978ab1adc218e3e867d73e397b18568e899
Shadab Zafar <dufferzafar0@gmail.com>
2015-09-18 14:27:28 +0530

    Minor change in the footer
    for the lulz.

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
       <footer class="footer">
-        <p>Made with <span class="glyphicon glyphicon-heart"></span> by Ameen Mohd Khan :)</p>
+        <p>Made with <span class="glyphicon glyphicon-heart"></span> in Django by Ameen Khan :)</p>
       </footer>
     </div>  <!-- Top level container -->
   </body>
