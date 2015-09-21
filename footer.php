ab956a428192c4f2abaf032f98413c5e1ffd3f73
Shadab Zafar <dufferzafar0@gmail.com>
2015-09-21 19:14:29 +0530

    Add real credits to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
       <footer class="footer">
-        <p>Made with <span class="glyphicon glyphicon-heart"></span> in Django by Ameen Khan :)</p>
+        <p>Made with <span class="glyphicon glyphicon-heart"></span> in <a href="http://github.com/dufferzafar/resuming">PHP</a> by <a href="http://github.com/kwikadi/">@kwikadi</a> and <a href="http://github.com/dufferzafar/">@dufferzafar</a> :)</p>
       </footer>
     </div>  <!-- Top level container -->
   </body>
